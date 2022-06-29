24b867514ac2bcf2b1b0da57cdce5eeeee504afa
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-06-29 13:43:42 -0400

    seudo coding

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,25 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// create generatePassword function.
+function generatePassword(){
+  console.log("confirm test");
+  
+  //prompt for password 
+  // 8 < 128 length
+  // lower,upper,number,special character
+
+  //validate the input.
+  // generate password
+  //display the password.
+
+
+
+
+
+  return "generated password value";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,3 +34,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
