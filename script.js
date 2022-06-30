257994880acea3761331cd7183eda8591bd98c99
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-06-30 18:56:22 -0400

    reorganize folders

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Assignment code here
+var charactersLength = 8;
+
+var numberArray = ["0","1","2","3","4","5","6","7","8","9"];
+var upperCaseArray = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+var lowerCaseArray = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+var symbolArray = ["!","@","#","$","%","^","&","*","(","?","/",];
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var charactersLenght = lenght
 
 // create generatePassword function.
 function generatePassword(){
   console.log("confirm test");
+  
   
   //prompt for password 
   // 8 < 128 length
@@ -34,5 +42,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
